daf98ab0588bc12f2dc5bae4ce54e8ab3d5dc1b2
baseebeck <baseebeck@gmail.com>
2020-11-19 14:47:34 -0500

    Updated project tiles to more mobile-friendly wrap

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 function expand() {
-  let hiddenCards = document.getElementsByClassName("hiddenCard");
+  let hiddenCards = document.querySelector(".hiddenCards");
   let expandText = document.querySelector("#expandText");
   let downArrow = document.querySelector(".downArrow");
   let upArrow = document.querySelector(".upArrow");
 
   if (expandText.innerHTML === "MORE PROJECTS") {
-    for (let i = 0; i < hiddenCards.length; i++) {
-      hiddenCards[i].classList.remove("hidden");
-    }
+    // for (let i = 0; i < hiddenCards.length; i++) {
+    //   hiddenCards[i].classList.remove("hidden");
+    // }
     expandText.innerHTML = "LESS PROJECTS";
     downArrow.classList.add("hidden");
     upArrow.classList.remove("hidden");
+    hiddenCards.classList.remove("hidden");
   } else {
-    for (let i = 0; i < hiddenCards.length; i++) {
-      hiddenCards[i].classList.add("hidden");
-    }
+    // for (let i = 0; i < hiddenCards.length; i++) {
+    //   hiddenCards[i].classList.add("hidden");
+    // }
     expandText.innerHTML = "MORE PROJECTS";
     downArrow.classList.remove("hidden");
     upArrow.classList.add("hidden");
+    hiddenCards.classList.add("hidden");
   }
 }
 
 document.querySelector(".portfolioExpand").onclick = function() {
   expand();
 }
+
+
+
+// function hiddenCards() {
+//     let hiddenCards = document.querySelector('.hiddenCards');
+//     if (hiddenCards.clientHeight) {
+//       hiddenCards.style.height = 0;
+//     } else {
+//
+//       hiddenCards.style.height = hiddenCards.scrollHeight + "px";
+//     }
+//
+//     let moreBtn = document.querySelector("#more-button");
+//     if (moreBtn.value == "LESS PROJECTS") {
+//
+//     }
+//
+// document.getElementById("more-button").value=document.getElementById("more-button").value=='MORE PROJECTS'?'LESS PROJECTS':'MORE PROJECTS';
+// }
